996c82534a6710de4d503a423ef624b1b9c4d32c
Kaue Marques Magnus <kmarquesmagnus@rede.ulbra.br>
2024-01-24 09:19:37 -0300

    add: estruturas condicionais

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,24 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+
+        /* Imagina que fomos requisitados a criar um sistema de plano telefônico onde:
+        O sistema terá 03 planos: BASIC, MIDIA , TURBO;
+        BASIC: 100 minutos de ligação;
+        MÍDIA: 100 minutos de ligação + WhatsApp e Instagram grátis;
+        TURBO: 100 minutos de ligação + WhatsApp e Instagram grátis + 5 GB Youtube. */
+
+        String plano = "M"; // M / T
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M": {
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
