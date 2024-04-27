c875fece3f88b59b16736decf475d38e5c50db64
KaueMagnus <kmarquesmagnus@gmail.com>
2024-04-27 19:59:50 -0300

    add: Interfaces POO

---
+++ POO/Interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,22 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+    @Override
+    public void copiar() {
+        System.out.println("Copiando via Equipamento MultiFuncional");
+    }
+
+    @Override
+    public void digitalizar() {
+        System.out.println("Digitalizando via Equipamento MultiFuncional");
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Imprimindo via Equipamento MultiFuncional");
+    }
+}
